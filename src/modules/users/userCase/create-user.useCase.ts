7cec8f642c1dd5bb5ec1a9fcdf4eed5835ee9d63
Diego Lima <diegogoncalvesreisdelima21@gmail.com>
2024-08-28 17:23:29 -0300

    feat(user): adicionar validação para prevenir usuários duplicados

--- src/modules/users/userCase/create-user.useCase.ts
+++ src/modules/users/userCase/create-user.useCase.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/infra/database/prisma.service';
 import { CreateUserDto } from '../dtos/user.dto';
 
@@ -14,7 +14,7 @@ export class CreateUserUserCase {
     });
 
     if (userAlreadyExist) {
-      throw new Error('User already exists!');
+      throw new HttpException('users already exist!', HttpStatus.BAD_REQUEST);
     }
 
     const user = await this.prisma.user.create({
